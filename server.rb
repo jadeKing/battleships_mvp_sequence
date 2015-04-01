e8c51e72c9d62de2232290b8bb25284abe109879
Augustinas Markevicius <augustinas.markevicius@gmail.com>
2015-04-01 18:00:25 +0100

    Basic user registration and firing at the board enabled

--- server.rb
+++ server.rb
@@ -1,14 +1,38 @@
 require 'sinatra/base'
+require_relative './lib/player'
+require_relative './lib/destroyer'
+require_relative './lib/submarine'
 
 class Battleshipsweb1 < Sinatra::Base
+
+  enable :sessions
+
+  PLAYER = Player.new
+
   get '/' do
     erb :index
   end
 
-  post '/game' do
-    @name = params[:title]
+  get '/game' do
+    @name = session[:title]
+    @hits = PLAYER.hits.length
+    @misses = PLAYER.misses.length
     erb :game
   end
+
+  post '/game' do
+    @name = params[:title]
+    session[:title] = @name
+    @hits = PLAYER.hits.length
+    @misses = PLAYER.misses.length
+    puts session.inspect
+    erb :game
+  end
+
+  post '/fire' do
+    PLAYER.receive_hit(params[:coord_input])
+  end
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
